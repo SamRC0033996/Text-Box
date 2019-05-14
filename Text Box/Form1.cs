79d22c14169e7765d28029e92b1e36cf42ae9dec
Sam Titterington <rc0033996@student.riversidecollege.ac.uk>
2019-05-14 10:11:46 +0100

    1

--- Text Box/Form1.cs
+++ Text Box/Form1.cs
@@ -15,6 +15,23 @@ namespace Text_Box
         public Form1()
         {
             InitializeComponent();
+            int i = 3;
+            //richTextBox1.AppendText(" " + i);
+
+            double pi = 3.141592653589793;
+            //richTextBox1.AppendText(pi.ToString());
+            //richTextBox1.AppendText(3.ToString());
+
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3")+"\n");
+            richTextBox1.AppendText(d1.ToString("N0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
         }
+        
     }
 }
